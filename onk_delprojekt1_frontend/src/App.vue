1b7dad3ceb7a47ead3f35cfb9d64145ebc32c719
Berthelmaster <Berthel0802@gmail.com>
2020-02-20 11:31:37 +0100

    Deleted standard page

--- onk_delprojekt1_frontend/src/App.vue
+++ onk_delprojekt1_frontend/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Frontpage/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Frontpage from './components/Frontpage.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Frontpage
   }
 }
 </script>
