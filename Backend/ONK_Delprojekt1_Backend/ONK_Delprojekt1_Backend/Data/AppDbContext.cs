97ffeb1c8f27d2f95ddf7f451995ebea27db509f
florent417 <flole@live.dk>
2020-02-20 12:59:59 +0100

    First attempt at database

---
+++ Backend/ONK_Delprojekt1_Backend/ONK_Delprojekt1_Backend/Data/AppDbContext.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using ONK_Delprojekt1_Backend.Models;
+using System.Configuration;
+using System.Data.SqlClient;
+using MySql.Data.EntityFrameworkCore;
+//using RestApi;
+
+namespace ONK_Delprojekt1_Backend.Data
+{
+    public class AppDbContext : DbContext
+    {
+            public AppDbContext(DbContextOptions options)
+                :base(options)
+            {
+            }
+
+            public DbSet<Haandvaerker> Haandvaerkerer { get; set; }
+            public DbSet<Vaerktoej> Vaerktoejer { get; set; }
+            public DbSet<Vaerktoejskasse> Vaerktoejskasser{ get; set; }
+            
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                //var connectionString = ConfigurationManager.ConnectionStrings["DitStamtrae"].ConnectionString;
+                optionsBuilder.UseMySQL("server=localhost;Data Source=testONK.db");
+            }
+
+            protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {
+                modelBuilder.Entity<Vaerktoej>()
+                    .HasOne(vk => vk.LiggerIvtkNavigation)
+                    .WithMany(v => v.Vaerktoej);
+
+                modelBuilder.Entity<Vaerktoejskasse>()
+                    .HasOne(h => h.EjesAfNavigation)
+                    .WithMany(vk => vk.Vaerktoejskasse);
+            }
+
+        }
+}
