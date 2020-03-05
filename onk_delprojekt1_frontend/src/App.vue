f258972032ae3ab418ea0cebda99e29c564022ba
Berthelmaster <Berthel0802@gmail.com>
2020-03-05 09:59:21 +0100

    added pages

--- onk_delprojekt1_frontend/src/App.vue
+++ onk_delprojekt1_frontend/src/App.vue
@@ -6,29 +6,15 @@
       dark
     >
       <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+      <h1>Den bedste ONK App</h1>  
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+      
       </div>
 
       <v-spacer></v-spacer>
 
       <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        href="https://github.com/Berthelmaster/ONK-Delopgave-Projekt/tree/master"
         target="_blank"
         text
       >
