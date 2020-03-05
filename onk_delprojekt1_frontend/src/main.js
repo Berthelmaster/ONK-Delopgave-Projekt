025fec0b81c7823453b58a3300ed9c5d110f3d24
Berthelmaster <Berthel0802@gmail.com>
2020-03-05 10:41:05 +0100

    vue router added

--- onk_delprojekt1_frontend/src/main.js
+++ onk_delprojekt1_frontend/src/main.js
@@ -1,10 +1,39 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
+import VueRouter from 'vue-router'
+import Create from './views/Create.vue'
+import Delete from './views/Delete.vue'
+import Read from './views/Read.vue'
+import Update from './views/Update.vue'
+import Frontpage from './views/Frontpage.vue'
 
 Vue.config.productionTip = false
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  routes: [
+    {
+      path: '', component: Frontpage
+    },
+    {
+      path: '/Create', component: Create
+    },
+    {
+      path: '/Delete', component: Delete
+    },
+    {
+      path: '/Read', component: Read
+    },
+    {
+      path: '/Update', component: Update
+    }
+  ],
+  mode: 'history'
+})
 
 new Vue({
   vuetify,
-  render: h => h(App)
+  router,
+  render: h => h(App),
 }).$mount('#app')
